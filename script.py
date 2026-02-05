cf8edad6fab05ddeef1bc613b58c999a7c880d24
egor <flufypro@gmail.com>
2026-02-05 14:59:21 +0800

    changed def

--- script.py
+++ script.py
@@ -4,4 +4,7 @@ def sum(a,b):
 def devide(a, b):
 	if b== 0:
 		raise ValueError("Denominator could not be zero")
+	if a isinstance(a, str) or isinstance(b, str):
+		raise ValueError("Could not devide strings")
 	return a/b
+#changes
